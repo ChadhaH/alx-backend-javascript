349ed7eafb61b6816da735f10f36faee673d0c8a
ChadhaH <chadha.hamed1@gmail.com>
2024-04-12 12:21:12 -0700

    fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,15 +4,9 @@ export default async function asyncUploadUser() {
   try {
     const resUploadPhoto = await uploadPhoto();
     const resCreateUser = await createUser();
-    return {
-      photo: resUploadPhoto,
-      user: resUploadPhoto,
-    };
-  } catch
-  (err) {
-    return {
-      photo: null,
-      user: null,
-    };
-  }
+    
+    return ({ photo: prom1, user: prom2 });
+   } catch (error) {
+     return ({ photo: null, user: null });
+   }
 }
